677b52b69b4adca8e563a67e88e1e57b156e4d2b
Yandra Muslim <yandrayt1@gmail.com>
2023-06-26 21:20:49 +0700

    changing path image

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,16 +1,16 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'images/')
-    },
-    filename: function (req, file, cb) {
-        // validatin mime type in here
-      const uniqueSuffix = Date.now().toString()
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, "/home/vijayasena/public_html/");
+  },
+  filename: function (req, file, cb) {
+    // validatin mime type in here
+    const uniqueSuffix = Date.now().toString();
+    cb(null, file.originalname);
 
-    //   false 
-    }
-  });
+    //   false
+  },
+});
 
-export default multer({ storage: storage }).single("gambar")
+export default multer({ storage: storage }).single("gambar");
